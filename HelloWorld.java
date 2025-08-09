872c56dfa5885d9d83a5123a279c4a4e75d6019d
intellectgit <125780936+intellectgit@users.noreply.github.com>
2025-08-09 18:03:44 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Poll SCM task ");
+         System.out.println("Webhooks has been triggered");
     }
 }
